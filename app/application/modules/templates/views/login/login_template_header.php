bf4f469d37433bc679bfbe32dd961fe25d39ef6c
awaisfiaz <awais.fayaz@upgenicsint.com>
2019-07-09 16:43:38 +0500

    Changes made

--- app/application/modules/templates/views/login/login_template_header.php
+++ app/application/modules/templates/views/login/login_template_header.php
@@ -6,6 +6,6 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 ?>
 
 <div id="header">	
-	<h1>login module by <a href="http://lewnelson.com">lewis nelson</a></h1>
-	<h2>built for CodeIgniter with <a href="https://bitbucket.org/wiredesignz/codeigniter-modular-extensions-hmvc" target="_blank">MX extensions</a> by Wiredesignz</h2>
+	<h1>login module for hmvc</h1>
+	<h2>built for CodeIgniter</h2>
 </div>
